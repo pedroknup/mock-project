dbe773a1fa0751d9c4c3bb24a2f76982095aca92
Jira-GPT <jira-gpt@gmail.com>
2023-05-13 10:21:39 +0000

    Removed button in HelloWorld component

--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -6,15 +6,10 @@ class HelloWorld extends React.Component {
         this.state = { message: 'Hello, World!' };
     }
 
-    handleClick = () => {
-        this.setState({ message: 'Button clicked!' });
-    }
-
     render() {
         return (
             <div>
                 <h1>{this.state.message}</h1>
-                <button onClick={this.handleClick}>Click me</button>
             </div>
         );
     }
