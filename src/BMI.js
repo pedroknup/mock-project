12ada91b7b8517bd49401afc37d630774320149d
Jira-GPT <jira-gpt@gmail.com>
2023-05-15 15:58:44 +0000

    Remove debug logs from BMI calculator

--- src/BMI.js
+++ src/BMI.js
@@ -1,18 +1,8 @@
 export const getAverageBodyMassIndex = (people) => {
-    try {
-        const totalBodyMassIndex = people.reduce((total, person) => {
-            console.log('person: ', person);
-            const bodyMassIndex = person.weight / (person.height * person.height);
-            console.log('body mass indexx: ', bodyMassIndex);
-            return total + bodyMassIndex;
-        }, 0);
+    const totalBodyMassIndex = people.reduce((total, person) => {
+        const bodyMassIndex = person.weight / (person.height * person.height);
+        return total + bodyMassIndex;
+    }, 0);
 
-        console.log('testing')
-
-        return totalBodyMassIndex / people.length;
-    } catch (error) {
-        console.log('It was not possible to calculate the average body mass index');
-        return error;
-    }
+    return totalBodyMassIndex / people.length;
 }
-
